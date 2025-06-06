f03093976b971b53a9c8de355886f49ebd5a15d3
Arseni Salavei <arsenisoloveii@gmail.com>
2025-06-06 21:24:16 +0200

    feat: db context class

---
+++ apbd-lab-12/Data/AppDbContext.cs
@@ -0,0 +1,49 @@
+﻿using apbd_lab_12.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace apbd_lab_12.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+    
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Trip> Trips { get; set; }
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<ClientTrip> ClientTrips { get; set; }
+    public DbSet<CountryTrip> TripCountries { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // client trip
+        modelBuilder.Entity<ClientTrip>()
+            .HasKey(ct => new { ct.IdClient, ct.IdTrip });
+
+        modelBuilder.Entity<ClientTrip>()
+            .HasOne(ct => ct.Client)
+            .WithMany(c => c.ClientTrips)
+            .HasForeignKey(ct => ct.IdClient);
+
+        modelBuilder.Entity<ClientTrip>()
+            .HasOne(ct => ct.Trip)
+            .WithMany(t => t.ClientTrips)
+            .HasForeignKey(ct => ct.IdTrip);
+
+        // trip country
+        modelBuilder.Entity<CountryTrip>()
+            .HasKey(tc => new { tc.IdTrip, tc.IdCountry });
+
+        modelBuilder.Entity<CountryTrip>()
+            .HasOne(tc => tc.Trip)
+            .WithMany(t => t.TripCountries)
+            .HasForeignKey(tc => tc.IdTrip);
+
+        modelBuilder.Entity<CountryTrip>()
+            .HasOne(tc => tc.Country)
+            .WithMany(c => c.TripCountries)
+            .HasForeignKey(tc => tc.IdCountry);
+    }
+}

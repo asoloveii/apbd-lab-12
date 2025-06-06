1672a1d51cbc9c493ee7e1b5742f112c09debfef
Arseni Salavei <arsenisoloveii@gmail.com>
2025-06-06 21:19:49 +0200

    feat: dtos completed

---
+++ apbd-lab-12/DTOs/TripDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace apbd_lab_12.DTOs;
+
+public class TripDTO
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime DateFrom { get; set; }
+    public DateTime DateTo { get; set; }
+    public int MaxPeople { get; set; }
+    public List<CountryDTO> Countries { get; set; }
+    public List<ClientDTO> Clients { get; set; }
+}
+
+public class CountryDTO
+{
+    public string Name { get; set; }
+}
+
+public class ClientDTO
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}

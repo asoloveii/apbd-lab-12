bc7020ed86e608e89fad687196470965e5a29ef1
Arseni Salavei <arsenisoloveii@gmail.com>
2025-06-06 21:15:20 +0200

    feat: implemented models

---
+++ apbd-lab-12/Models/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace apbd_lab_12.Models;
+
+public class Country
+{
+    public int IdCountry { get; set; }
+    public string Name { get; set; }
+    
+    public virtual ICollection<CountryTrip> TripCountries { get; set; }
+}
---
+++ apbd-lab-12/Models/CountryTrip.cs
@@ -0,0 +1,10 @@
+﻿namespace apbd_lab_12.Models;
+
+public class CountryTrip
+{
+    public int IdCountry { get; set; }
+    public int IdTrip { get; set; }
+    
+    public virtual Trip Trip { get; set; }
+    public virtual Country Country { get; set; }
+}
